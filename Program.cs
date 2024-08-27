58a92c1bdc43ea931e445c3bccc2800c8b1a1a0a
Abdul-Quadir Rahman Taiwo <rahmanabdulquadir@gmail.com>
2024-08-26 17:26:05 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+
+// Assesment
+// 1
+// int a = 1;
+// int b = 2;
+// int c = 3;
+// int d = 4;
+// int sum;
+// sum  = a + b + c + d;
+// Console.WriteLine("The sum of the positive integers are : " + sum);
+
+
+// 2
+// int sum;
+// Console.WriteLine("Input first myNumber");
+// int myNumber1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input first value");
+// int myNumber2 = Convert.ToInt32(Console.ReadLine());
+
+// sum = myNumber1 + myNumber2;
+// Console.WriteLine("The sum of the two integers are : " + sum);
+
+
+
+// 3
+// Console.WriteLine("Input a number");
+// int myNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (myNumber % 2 == 0 )
+// {
+//     Console.WriteLine($"{myNumber} is Even Number");
+// }
+// else 
+// {
+//      Console.WriteLine($"{myNumber} is not Odd Number");
+// }
+
+
+// // 4
+Console.WriteLine("a");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("c");
+ int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("b");
+ int b = int.Parse(Console.ReadLine());
+
+if (a + b == 0) {
+    Console.WriteLine($"{a + b}");
+}
+else if (a - b == 0){
+    Console.WriteLine($"{a - b}");
+}
+else if (a % b != 0) {
+    Console.WriteLine($"{a % b}");
+}
+else if (a * b == 0) {
+    Console.WriteLine($"{a * b}");
+}
+
+// if (a * b == 0 || a + b == 0 || a / b == 0 || a - b == 0)
+// {
+//     Console.WriteLine(ab);
+// }
+ 
+
+
+// 5
+// Console.Write("Input valua a : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Input valua b : ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Input valua c : ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b && a > c ) {
+//     Console.WriteLine($"The largest input is {a}");
+// }
+// else if(b > a && b > c)
+// {
+//     Console.WriteLine($"The largest input is {b}");
+// }
+// else if(c > a && c > b)
+// {
+//     Console.WriteLine($"The largest input is {c}");
+// }
